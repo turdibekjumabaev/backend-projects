bb77f7120483db71f1d131f14f6fe23d60baf976
turdibek <turdibekjumabaev05@gmail.com>
2024-02-29 03:20:57 +0500

    initial commit

---
+++ medium/src/main/java/org/backend/medium/MediumApplication.java
@@ -0,0 +1,13 @@
+package org.backend.medium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediumApplication.class, args);
+    }
+
+}
---
+++ medium/src/test/java/org/backend/medium/MediumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend.medium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
